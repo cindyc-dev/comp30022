2a00b8c13bc95aa08a4ea0bff9c78c821ac62650
Xin Yu <chuaxc@unimelb.edu.au>
2023-08-07 15:28:43 +1000

    Resolve API handler error
    * Error message: `should not return a value, received object.`
    * Solution: https://stackoverflow.com/questions/76547978/next-auth-gives-error-on-sign-in-api-handler-should-not-return-a-value-receiv

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
 
-export default NextAuth(authOptions);
+const authHandler = NextAuth(authOptions);
+export default async function handler(...params: any[]) {
+  await authHandler(...params);
+}
