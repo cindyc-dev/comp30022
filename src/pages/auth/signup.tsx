4c0e455ff601f0cbbfe678c2bccbbced6b26d260
Xin Yu <chuaxc@unimelb.edu.au>
2023-08-10 19:12:51 +1000

    Added DaisyUI and react-icons; did styling for Sign Up page

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,84 +1,105 @@
-import React, { useRef } from "react";
+import dynamic from "next/dynamic";
+import { useRef, useState } from "react";
+
+import PasswordInput from "~/components/passwordInput";
+
+const PasswordChecklist = dynamic(() => import("react-password-checklist"), {
+  ssr: false,
+});
 
 // TODO use React Hook Forms instead of this
 
 export default function SignUp() {
-  const username = useRef<HTMLInputElement>(null);
   const email = useRef<HTMLInputElement>(null);
-  const password = useRef<HTMLInputElement>(null);
-  const confirmPassword = useRef<HTMLInputElement>(null);
-  const [error, setError] = React.useState<string | null>(null);
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (password.current?.value !== confirmPassword.current?.value) {
-      setError("Passwords do not match.");
-      return;
-    }
-    setError(null);
     const user = {
-      username: username.current?.value,
-      password: password.current?.value,
+      email: email.current?.value,
+      password: password,
     };
     console.log({ user: user });
   };
   return (
-    <div>
-      SignUp
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <label>
-          Username
-          <input
-            name="username"
-            type="text"
-            placeholder="Username"
-            required
-            ref={username}
-          />
-        </label>
-        <label>
-          Email
-          <input
-            name="email"
-            type="email"
-            placeholder="Email"
-            required
-            ref={email}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            name="password"
-            type="password"
-            placeholder="*******"
-            autoComplete="new-password"
-            required
-            ref={password}
-          />
-        </label>
-        <label>
-          Confirm Password
-          <input
-            name="confirmPassword"
-            type="password"
-            placeholder="*******"
-            autoComplete="new-password"
-            required
-            ref={confirmPassword}
-          />
-        </label>
-        <button type="submit">Sign Up</button>
-      </form>
-      <p>
-        Already have an account? <a href="/auth/signin">Log In.</a>
-      </p>
-      {error && (
-        <p>
-          ERROR
-          <br />
-          {error}
-        </p>
-      )}
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="p-10 text-center lg:text-left">
+          <h1 className="text-5xl font-bold">PotatoCRM</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
+          <div className="card-body">
+            <h2 className="card-title m-auto">Sign Up!</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="example@example.com"
+                  className="input input-bordered"
+                  ref={email}
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <PasswordInput setValue={setPassword} />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Confirm Password</span>
+                </label>
+                <input
+                  className="input input-bordered"
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="*******"
+                  autoComplete="new-password"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mt-2">
+                <PasswordChecklist
+                  rules={[
+                    "minLength",
+                    "specialChar",
+                    "number",
+                    "capital",
+                    "match",
+                  ]}
+                  minLength={8}
+                  value={password}
+                  valueAgain={confirmPassword}
+                  onChange={(isValid) => console.log({ isValid })}
+                  className="text-xs"
+                />
+              </div>
+
+              <div className="form-control mt-6">
+                <button className="btn btn-primary" type="submit">
+                  Sign Up
+                </button>
+              </div>
+            </form>
+            <p className="text-sm">
+              Already have an account?{" "}
+              <a className="link-primary link" href="/auth/signin">
+                Log In.
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
