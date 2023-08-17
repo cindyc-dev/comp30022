79c5972a7f7cda642772baf6418ef692005e7dbe
Xin Yu <chuaxc@unimelb.edu.au>
2023-08-17 15:24:49 +1000

    Added Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,38 @@
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+
 export const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className="footer footer-center bg-primary py-2 text-primary-content">
+      <div>
+        <div className="flex gap-4">
+          <p>Made with Awesomeness 🤟 for COMP30022</p>
+          <Link href="https://github.com/chuahxinyu/comp30022">
+            <FaGithub className="text-xl" />
+          </Link>
+        </div>
+        <p>
+          <a className="link underline" href="">
+            Ashley Teoh
+          </a>
+          ,{" "}
+          <a className="link underline" href="">
+            Aurelia Iskandar
+          </a>
+          ,{" "}
+          <a className="link underline" href="">
+            Cindy Chuah Xin Yu
+          </a>
+          ,{" "}
+          <a className="link underline" href="">
+            Daniel Chin Weng Jae
+          </a>
+          ,{" "}
+          <a className="link underline" href="">
+            Gan Yu Pin
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 };
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import { Navbar } from "./navbar";
+import { Footer } from "./footer";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
@@ -8,6 +9,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
       <div className="container prose m-auto flex flex-grow flex-col items-center justify-center">
         {children}
       </div>
+      <Footer />
     </div>
   );
 };
