f41327cd635c0866e57e4ba47d00e37cc9a4018b
Xin Yu <chuaxc@unimelb.edu.au>
2023-08-17 13:50:40 +1000

    Merge Sign In Page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,6 +1,3 @@
-// Custom Sign In Page
-// Reference: https://next-auth.js.org/configuration/pages
-
 import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
@@ -15,45 +12,70 @@ export default function SignIn({
   csrfToken,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <>
-      {providers &&
-        Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
-      <form>
-        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-        <label>
-          Username
-          <input name="username" type="text" placeholder="Username" />
-        </label>
-        <label>
-          Password
-          <input
-            name="password"
-            type="password"
-            placeholder="*******"
-            autoComplete="new-password"
-          />
-        </label>
-        <button type="submit">Sign in</button>
-        <p>
-          New to PotatoCRM? <Link href="/auth/signup">Create an account.</Link>
-        </p>
-      </form>
-    </>
+    <div className="flex min-h-screen bg-gray-100">
+      <div className="flex w-[25%] flex-col items-center justify-center bg-white p-8">
+        <form className="w-[80%]">
+          <h1 className="mb-1"> Welcome Back</h1>
+          <p className="mt-1 text-left">
+            New to PotatoCRM?{" "}
+            <Link href="/auth/signup">Create an account.</Link>;
+          </p>
+          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+          <label className="mb-2 mt-8 block font-bold">
+            Username
+            <input
+              className="mt-1 block w-full rounded-lg border px-2 py-2"
+              name="username"
+              type="text"
+              placeholder="Username"
+            />
+          </label>
+          <label className="mb-11 block font-bold">
+            Password
+            <input
+              className="mt-1 block w-full rounded-lg border px-2 py-2"
+              name="password"
+              type="password"
+              placeholder=" *******"
+              autoComplete="new-password"
+            />
+          </label>
+          <button
+            className="mb-4 w-full rounded-lg border-gray-400 bg-purple-100 py-2 text-white hover:bg-blue-600"
+            type="submit"
+          >
+            Sign in
+          </button>
+          <button className="btn btn-primary " type="submit">
+            Sign in
+          </button>
+          {providers &&
+            Object.values(providers).map((provider) => (
+              <div key={provider.name}>
+                <button
+                  className="w-full rounded-lg border-gray-400 bg-white p-2 font-bold text-black hover:bg-blue-600"
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign in with {provider.name}
+                </button>
+              </div>
+            ))}
+        </form>
+      </div>
+      <div className="flex w-[75%] bg-blue-50">
+        {/* <img
+        src="back.png"  
+        alt="Description of the image"
+        className="max-w-full h-auto"
+      /> */}
+      </div>
+    </div>
   );
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getServerSession(context.req, context.res, authOptions);
 
-  // If the user is already logged in, redirect.
-  // Note: Make sure not to redirect to the same page
-  // To avoid an infinite loop!
   if (session) {
     return { redirect: { destination: "/dashboard" } };
   }
