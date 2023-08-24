f56f6f9a4617e8c767ff2a5870b692e6c6079911
Xin Yu <chuaxc@unimelb.edu.au>
2023-08-24 13:34:32 +1000

    Fix Background image and hero text colour

--- src/components/authLayout.tsx
+++ src/components/authLayout.tsx
@@ -1,19 +1,20 @@
+import Image from "next/image";
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div
-      className="hero min-h-screen bg-base-200"
-      style={{
-        backgroundImage: "url(${bg.src})",
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-      }}
-    >
+    <div className="hero min-h-screen bg-base-200">
+      <Image
+        src="/images/background.png"
+        alt="background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+      />
       <div className="hero-content flex-col lg:flex-row">
-        <div className="p-10 text-center lg:text-left">
+        <div className="p-10 text-center text-base-100 lg:text-left">
           <h1 className="text-5xl font-bold">PotatoCRM</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
