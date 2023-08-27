2ac32c95454b0620449b0431c6a3f34050b76062
Xin Yu <chuaxc@unimelb.edu.au>
2023-08-28 00:57:57 +1000

    Fix linting issue

--- src/pages/connections/index.tsx
+++ src/pages/connections/index.tsx
@@ -153,9 +153,9 @@ export default function Connections() {
                       {header.isPlaceholder
                         ? null
                         : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
                     </th>
                   ))}
                 </tr>
