ca25fc70eb1277fe824bea6e1dd1940a523bc6bb
Xin Yu <chuaxc@unimelb.edu.au>
2023-08-27 19:00:54 +1000

    Fix Styling and Colour of Navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { FaCalendarAlt } from "react-icons/fa";
+import { FaBars, FaCalendarAlt } from "react-icons/fa";
 import { BsKanbanFill, BsFillPeopleFill } from "react-icons/bs";
 import { AvatarImage } from "../common/avatarImage";
 
@@ -28,35 +28,26 @@ export const Navbar = () => {
   const currentPage = router.pathname;
 
   return (
-    <div className="navbar sticky top-0 z-50 bg-base-100 shadow">
+    <div className="navbar sticky top-0 z-50 bg-neutral text-neutral-content shadow">
       <div className="navbar-start">
         {/* Hamburger Dropdown for Smaller Phones */}
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+            <FaBars />
           </label>
           <ul
             tabIndex={0}
-            className="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
+            className="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-neutral p-2 shadow"
           >
             {PAGES.map(({ icon, path, name }) => (
               <li key={name}>
                 <Link
                   href={path}
-                  className={currentPage === path ? "focus" : ""}
+                  className={
+                    currentPage === path
+                      ? "bg-neutral-focus !text-neutral-content"
+                      : "!text-neutral-content"
+                  }
                 >
                   {icon}
                   {name}
@@ -66,10 +57,7 @@ export const Navbar = () => {
           </ul>
         </div>
         {/* Logo */}
-        <Link
-          className="px-2 text-xl font-bold normal-case text-primary"
-          href="/dashboard"
-        >
+        <Link className="px-2 text-xl font-bold normal-case" href="/dashboard">
           PotatoCRM
         </Link>
       </div>
@@ -77,7 +65,14 @@ export const Navbar = () => {
         <ul className="menu menu-horizontal gap-2 px-1">
           {PAGES.map(({ icon, path, name }) => (
             <li key={name}>
-              <Link href={path} className={currentPage === path ? "focus" : ""}>
+              <Link
+                href={path}
+                className={
+                  currentPage === path
+                    ? "bg-neutral-focus !text-neutral-content"
+                    : "!text-neutral-content"
+                }
+              >
                 {icon}
                 {name}
               </Link>
@@ -94,13 +89,24 @@ export const Navbar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
+            className="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-neutral p-2 shadow"
           >
             <li>
-              <Link href="/profile">Profile</Link>
+              <Link
+                href="/profile"
+                className={
+                  currentPage === "/profile"
+                    ? "bg-neutral-focus !text-neutral-content"
+                    : "!text-neutral-content"
+                }
+              >
+                Profile
+              </Link>
             </li>
             <li>
-              <a>Logout</a>
+              <Link href="/auth/logout" className="!text-neutral-content">
+                Log Out
+              </Link>
             </li>
           </ul>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,24 @@ import { type Config } from "tailwindcss";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {}
-    },
+  daisyui: {
+    themes: [
+      {
+        potatotheme: {
+          primary: "#626FB5",
+          secondary: "#44529B",
+          accent: "#282B40",
+          neutral: "#626FB5",
+          "neutral-content": "#ffffff",
+          "neutral-focus": "#44529B",
+          "base-100": "#ffffff",
+          info: "#85C1F9",
+          success: "#96D9A9",
+          warning: "#F9D885",
+          error: "#F98585",
+        },
+      },
+    ],
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 } satisfies Config;
