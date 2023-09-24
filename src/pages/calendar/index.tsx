b47b1fd0041941be163303b1512541a0ebafca33
Xin Yu <chuaxc@unimelb.edu.au>
2023-09-24 14:41:18 +1000

    Added Week Grid

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -109,6 +109,12 @@ export default function Calendar() {
   );
 }
 
+const arrayRange = (start: number, stop: number, step: number) =>
+  Array.from(
+    { length: (stop - start) / step + 1 },
+    (value, index) => start + index * step
+  );
+
 function WeekView({
   today,
   setToday,
@@ -122,7 +128,7 @@ function WeekView({
     <div className="w-full">
       <div className="sticky top-16 z-30 h-2 bg-base-100"></div>
       <div
-        className="sticky top-[4.5rem] z-40 grid w-full place-content-center gap-1 rounded bg-secondary p-2"
+        className="sticky top-[4.5rem] z-40 grid w-full place-content-center rounded bg-secondary py-2"
         style={{
           gridTemplateColumns: "3em 10px repeat(7, 1fr)",
           boxShadow: "0px 15px 10px -15px #ececec",
@@ -135,54 +141,59 @@ function WeekView({
         {Array.from(Array(7).keys()).map((day) => {
           const currDate = today.startOf("week").clone().add(day, "day");
           return (
-            <div
-              key={day}
-              className="flex w-full flex-col gap-1 border-l-2 border-base-100"
-            >
-              <div className="flex w-full justify-center">
-                <p className="m-0">
-                  {today
-                    .startOf("week")
-                    .clone()
-                    .add(day, "day")
-                    .format("ddd")
-                    .toUpperCase()}
-                </p>
-              </div>
-              <div className="flex w-full justify-center">
-                <button
-                  className={`btn btn-circle ${
-                    today.isSame(currDate) ? "btn-primary" : "btn-ghost"
-                  }`}
-                  onClick={() => {
-                    setView("day");
-                    setToday(currDate);
-                  }}
-                >
-                  {currDate.format("D")}
-                </button>
-              </div>
+            <div key={day} className="border-l-2 border-base-100 text-center">
+              <p className="m-0">
+                {today
+                  .startOf("week")
+                  .clone()
+                  .add(day, "day")
+                  .format("ddd")
+                  .toUpperCase()}
+              </p>
+              <button
+                className={`btn btn-circle ${
+                  today.isSame(currDate) ? "btn-primary" : "btn-ghost"
+                }`}
+                onClick={() => {
+                  setView("day");
+                  setToday(currDate);
+                }}
+              >
+                {currDate.format("D")}
+              </button>
             </div>
           );
         })}
       </div>
       <div
-        className="grid"
+        className="grid w-full"
         style={{
           gridTemplateColumns: "3em 10px repeat(7, 1fr)",
-          gridTemplateRows: "repeat(24, 3em)",
+          gridTemplateRows: "repeat(48, 2em)",
         }}
       >
-        {/* Times from 00:00 to 00:00 */}
+        {/* Times from 00:00 to 23:00 */}
         {Array.from(Array(24).keys()).map((hour) => (
-          <div
-            key={hour}
-            className="bottom-[-1ex] text-right"
-            style={{ gridColumn: "1" }}
-          >
-            <p className="m-0">{hour}:00</p>
-          </div>
+          <>
+            <div className="row-span-2 text-right" style={{ gridColumn: "1" }}>
+              <p className="m-0">{hour}:00</p>
+            </div>
+            <div className="row-span-2 border-b-2  border-base-200"></div>
+          </>
         ))}
+        {/* Fill rows with borders */}
+        {arrayRange(3, 9, 1).map((col) =>
+          arrayRange(1, 48, 2).map((row) => (
+            <div
+              className="row-span-2 border-b-2 border-l-2 border-base-200"
+              style={{
+                gridColumn: col,
+                gridRowStart: row,
+                gridRowEnd: row + 2,
+              }}
+            ></div>
+          ))
+        )}
       </div>
     </div>
   );
