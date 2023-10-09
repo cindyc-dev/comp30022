5a53d1d42d0d6892e80dfd2e031ff05c16083b0c
ashleyteoh <teohashley@gmail.com>
2023-10-09 12:34:51 +1100

    completed connection router functions

---
+++ src/types/BEConnectionI.d.ts
@@ -0,0 +1,9 @@
+export interface BEConnectionI {
+    name: string;
+    phone?: string;
+    photoUrl?: string;
+    email: string;
+    tags: string;
+    notes?: string;
+    isExisting?: boolean;
+}
