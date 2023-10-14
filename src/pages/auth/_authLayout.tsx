1dc84c2636847d2907ac17497738de1729ab78dd
Aurelia <aiskandar@student.unimelb.edu.au>
2023-10-14 21:41:21 +1100

    finished carousel with blurbs

--- src/pages/auth/_authLayout.tsx
+++ src/pages/auth/_authLayout.tsx
@@ -3,11 +3,27 @@ import ToastSection from "~/components/common/toastSection";
 import { useToast } from "~/components/hooks/toastContext";
 import Image from "next/image";
 
-const images = [
-  "/images/Frame 1.png",
-  "/images/Frame 2.png",
-  "/images/Frame 3.png",
-  "/images/Frame 4.png",
+const CAROUSEL = [
+  {
+    image: "/images/Frame 1.png",
+    title: "Connections",
+    blurb: "Foster and manage valuable connections with our Connections page. Here, you can effortlessly keep track of your clients, leads, and contacts. Build and nurture relationships, store important contact details, and track interactions in one centralized hub. Strengthen your network and watch your CRM relationships flourish.",
+  },
+  {
+    image: "/images/Frame 2.png",
+    title: "Calendar",
+    blurb: "Stay organized and never miss a beat with our Calendar page. Keep track of important dates, meetings, and deadlines all in one place. Schedule, view, and manage your events with ease. Our interactive calendar ensures you're always on top of your commitments and can efficiently plan your days.",
+  },
+  {
+    image: "/images/Frame 3.png",
+    title: "Dashboard",
+    blurb: "Get a real-time overview of your business operations with our Dashboard page. Access key metrics, analytics, and insights at a glance. Customizable widgets allow you to tailor your dashboard to your specific needs, giving you a comprehensive view of your CRM data. Make data-driven decisions and stay in control of your business.",
+  },
+  {
+    image: "/images/Frame 4.png",
+    title: "Trello Board",
+    blurb: "Experience a comprehensive view of your daily workflow with our all-in-one Dashboard. Stay on top of your schedule with today's calendar. Monitor your Trello tasks and projects in real-time, helping you manage your workload efficiently. Access your contacts, fostering connections and tracking interactions, all in one centralized hub.",
+  }
 ];
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
@@ -15,18 +31,18 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const showNextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % CAROUSEL.length);
   };
 
   const showPreviousImage = () => {
     setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      prevIndex === 0 ? CAROUSEL.length - 1 : prevIndex - 1
     );
   };
 
   useEffect(() => {
-    // Automatically switch to the next image every 5 seconds
-    const intervalId = setInterval(showNextImage, 5000);
+    // Automatically switch to the next image every 4 seconds
+    const intervalId = setInterval(showNextImage, 4000);
 
     // Cleanup the interval on unmount to prevent memory leaks
     return () => clearInterval(intervalId);
@@ -35,28 +51,39 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <div className="p-10 text-center text-primary lg:text-left">
-          <div>
+        <div className="py-0 px-0  text-center text-primary lg:text-left">
+          <div className="w-full flex items-center justify-center flex-col">
+            <div className="flex gap-3">
+              <Image
+                src="/svg/favicon.svg"
+                alt="Potato Logo"
+                width={50}
+                height={0}
+                className="m-0 p-0"
+              />
+              <h1 className="text-5xl font-bold">PotatoCRM</h1>
+            </div>
             <Image
-              src={images[currentImageIndex]}
+              src={CAROUSEL[currentImageIndex].image}
               alt={`Image ${currentImageIndex + 1}`}
-              width={1920}
-              height={1080}
+              width={1000}
+              height={0}
             />
-            <div className="carousel-controls">
+            <div className="w-full flex items-center justify-center gap-5">
               <button className="prev" onClick={showPreviousImage}>
                 &#8249;
               </button>
+              {CAROUSEL.map((_, i) => (
+                <div key={i} className={`${i === currentImageIndex ? "w-3 h-3 bg-primary" : "w-2 h-2 bg-base-300"} rounded-full mt-1 cursor-pointer`} onClick={() => setCurrentImageIndex(i)}></div>
+              ))}
               <button className="next" onClick={showNextImage}>
                 &#8250;
               </button>
             </div>
           </div>
-          <h1 className="text-5xl font-bold">PotatoCRM</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          <h1 className="text-3xl font-bold">{CAROUSEL[currentImageIndex].title}</h1>
+          <p className="py-2">
+            {CAROUSEL[currentImageIndex].blurb}
           </p>
         </div>
         <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
