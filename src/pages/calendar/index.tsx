2269b26f0e91e30999477dd24b93b67f8b535e39
Jaee-C <77240633+Jaee-C@users.noreply.github.com>
2023-10-18 19:14:31 +1100

    remove debug messages

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -137,7 +137,6 @@ export default function Calendar() {
   /* Edit Event */
   const editMutation = api.calendar.editEvent.useMutation();
   const handleEditEvent = (event: EventI) => {
-    console.log(event);
     const newEvent = {
       id: event.id,
       title: event.title,
